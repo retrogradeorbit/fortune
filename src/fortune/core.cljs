445d9800cf8af622b9c099ee4021d754f61a835c
Crispin Wellington <retrogradeorbit@gmail.com>
2015-09-05 17:54:15 +0800

    define parabolas

--- src/fortune/core.cljs
+++ src/fortune/core.cljs
@@ -16,3 +16,22 @@
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
 
+
+(defn calculate-k
+  "Given the turning point of a parabola specified as pos1 (x1, x2),
+  and making it pass through second point pos2, return the
+  coefficient k, such that the parabola could be described by
+  y = k * (x - x1)^2 + y1"
+  [[x1 y1] [x2 y2]]
+  (/
+   (- y2 y1)
+   (*
+    (- x2 x1)
+    (- x2 x1))))
+
+(defn parabola-y
+  "Given a parabola [pos k], and a x position, x
+  return the y position from the described parabola"
+  [[[x1 y1] k] x]
+  (+ y1 (* k (- x x1) (- x x1))))
+
