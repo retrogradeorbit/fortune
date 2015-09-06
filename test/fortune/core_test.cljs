48f1c6af9ac95a7507c396fddde10baee527f89a
Crispin Wellington <retrogradeorbit@gmail.com>
2015-09-06 19:06:51 +0800

    tests for the functions

--- test/fortune/core_test.cljs
+++ test/fortune/core_test.cljs
@@ -1,3 +1,24 @@
 (ns fortune.core-test
   (:require [cljs.test :refer-macros [deftest is]]
             [fortune.core :as core]))
+
+(deftest calculate-k
+  (is (= (core/calculate-k [0 0] [-1 1]) 1))
+  (is (= (core/calculate-k [0 0] [1 1]) 1))
+  (is (= (core/calculate-k [0 0] [1 -1]) -1))
+  (is (= (core/calculate-k [0 0] [-1 -1]) -1))
+  (is (= (core/calculate-k [1 1] [2 3]) 2)))
+
+(deftest parabola-y
+  (is (= (core/parabola-y [[0 0] 1] 0) 0))
+  (is (= (core/parabola-y [[0 0] 1] 1) 1))
+  (is (= (core/parabola-y [[0 0] 1] 2) 4))
+  (is (= (core/parabola-y [[-10 -10] 1] -8) -6))
+  (is (= (core/parabola-y [[0 0] 2] -1) 2))
+  (is (= (core/parabola-y [[0 0] 3] -2) 12)))
+
+(deftest parabola-intersection
+  (is (= (core/parabola-intersection [[1 3] 1] [[0 0] 1]) #{2}))
+  (is (= (core/parabola-intersection [[0 0] 1] [[0 0] 2]) #{0}))
+  (is (= (core/parabola-intersection [[0 1] 1] [[0 0] 2]) #{-1 1}))
+  (is (= (core/parabola-intersection [[0 1] 1] [[0 0] 1]) #{})))
