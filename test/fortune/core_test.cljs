21e384f388c2d2a88c1672527a340bdc33875d4d
Crispin Wellington <retrogradeorbit@gmail.com>
2015-09-12 13:58:30 +0800

    new tests for functions

--- test/fortune/core_test.cljs
+++ test/fortune/core_test.cljs
@@ -29,3 +29,41 @@
   (is (= (core/circle-center [1 1] [-1 1] [-1 -1]) [0 0]))
   (is (= (core/circle-center [5 5] [6 -2] [2 -4]) [2 1]))
   )
+
+(deftest compute-parabola-from-point-and-sweep
+  (is (= (core/compute-parabola-from-point-and-sweep [0 10] 9)
+         [[0 9.5] 0.5]))
+  (is (= (core/compute-parabola-from-point-and-sweep [0 10] 8)
+         [[0 9.0] 0.25]))
+  (is (= (core/compute-parabola-from-point-and-sweep [5 10] 6)
+         [[5 8.0] 0.125])))
+
+(deftest sweep-parabola-intersection
+  (is (= (sort (core/sweep-parabola-intersection [0 1] [0 2] 0))
+         (list (- (Math/sqrt 2)) (Math/sqrt 2))))
+  (is (= (sort (core/sweep-parabola-intersection [5 2] [5 4] 0))
+         (list (- 5 (* 2 (Math/sqrt 2))) (+ 5 (* 2 (Math/sqrt 2)))))))
+
+(deftest compute-parabola
+  (let [[x1 x2] (vec (core/sweep-parabola-intersection [5 5] [10 10] 4))]
+    (is
+     (core/almost
+      (core/parabola-y (core/compute-parabola-from-point-and-sweep [5 5] 4) x1)
+      (core/parabola-y (core/compute-parabola-from-point-and-sweep [10 10] 4) x1)))
+    (is
+     (core/almost
+      (core/parabola-y (core/compute-parabola-from-point-and-sweep [5 5] 4) x2)
+      (core/parabola-y (core/compute-parabola-from-point-and-sweep [10 10] 4) x2)))))
+
+(deftest update-beach-intersections
+  (let [beach [[10 10] 0.5999 [6 9] 6.0001 [10 10]]
+        sweep 8
+        [p0 x0 p1 x1 p2] (core/update-beach-intersections beach sweep)]
+    (is (= p0 p2))
+    (is (< x0 x1))
+    (is (core/almost
+         (core/parabola-y (core/compute-parabola-from-point-and-sweep p0 sweep) x0)
+         (core/parabola-y (core/compute-parabola-from-point-and-sweep p1 sweep) x0)))
+    (is (core/almost
+         (core/parabola-y (core/compute-parabola-from-point-and-sweep p1 sweep) x1)
+         (core/parabola-y (core/compute-parabola-from-point-and-sweep p2 sweep) x1)))))
